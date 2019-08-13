679d4b72a925fc2118ed57f4302597c2472694eb
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-13 20:17:20 +0500

    Updates

---
+++ precache-manifest.21fe76bd6d8c690b2e64f0e716d640b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880c795962b08e96c6e5ce29b8b8c37e",
+    "url": "/monsters-search/index.html"
+  },
+  {
+    "revision": "eb7fed6c917413e76a68",
+    "url": "/monsters-search/static/css/main.7cfdd3cf.chunk.css"
+  },
+  {
+    "revision": "b0db3fcb9dfd9d99877c",
+    "url": "/monsters-search/static/js/2.5b3f03d1.chunk.js"
+  },
+  {
+    "revision": "eb7fed6c917413e76a68",
+    "url": "/monsters-search/static/js/main.1037d524.chunk.js"
+  },
+  {
+    "revision": "fa36fdea6c00bd9fe424",
+    "url": "/monsters-search/static/js/runtime~main.de126318.js"
+  }
+]);
